768f23372968b7ee4220b059b181004568d15dae
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2020-06-27 17:46:25 -0400

    initial commit

---
+++ src/main/java/com/full2cloud/pgptool/App.java
@@ -0,0 +1,13 @@
+package com.full2cloud.pgptool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/full2cloud/pgptool/AppTest.java
@@ -0,0 +1,20 @@
+package com.full2cloud.pgptool;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
